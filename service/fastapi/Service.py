3dda6c7038b11a82666a9471ea58a76f8e8bd293
antman1935 <anthony.lamont99@yahoo.com>
2024-04-01 13:14:41 -0400

    refactor and document endpoints

--- service/fastapi/Service.py
+++ service/fastapi/Service.py
@@ -2,13 +2,19 @@ from fastapi import FastAPI, Query
 from pydantic_settings import BaseSettings
 from contextlib import asynccontextmanager
 from typing_extensions import Annotated
+import inspect
 
 simServer = None
 
-def object_set_endpoint(object_name: str, attrs):
+def get_endpoint_parameters(attrs):
+    # replace periods to guarantee names are valid python vars,
+    # provide the mapping so that we can give the exact reference name
+    # NOTE: Possible TODO - do something about names resolving to the same
+    # alias. Issue is Ref_a100.Value and Ref.a100_Value both would have
+    # Ref_a100_Value as the base parameter name. We have to avoid these collisions
+    # manually or handle them explicitly.
     kwargs = {attr.replace('.', '_'): type_ for attr, type_ in attrs.items()}
     mapping = {attr.replace('.', '_'): attr for attr in attrs.keys()}
-    import inspect
     params = [
         inspect.Parameter(
             param,
@@ -17,6 +23,11 @@ def object_set_endpoint(object_name: str, attrs):
             default=None
         ) for param, type_ in kwargs.items()
     ]
+
+    return kwargs, mapping, params
+
+def object_set_endpoint(object_name: str, attrs):
+    kwargs, mapping, params = get_endpoint_parameters(attrs)
 
     async def endpoint(**kwargs):
         ref_settings = {f"{object_name}.{mapping[arg]}": kwargs[arg] for arg, val in kwargs.items() if val is not None}
@@ -33,17 +44,7 @@ def create_object_set_endpoint(app: FastAPI, object_name: str, attrs: list[str])
     app.add_api_route(f"/{object_name}/set/", object_set_endpoint(object_name, endpoint_params), methods=["GET"])
 
 def object_get_endpoint(object_name: str, attrs):
-    kwargs = {attr.replace('.', '_'): type_ for attr, type_ in attrs.items()}
-    mapping = {attr.replace('.', '_'): attr for attr in attrs.keys()}
-    import inspect
-    params = [
-        inspect.Parameter(
-            param,
-            inspect.Parameter.POSITIONAL_OR_KEYWORD,
-            annotation=type_,
-            default=True
-        ) for param, type_ in kwargs.items()
-    ]
+    kwargs, mapping, params = get_endpoint_parameters(attrs)
 
     async def endpoint(**kwargs):
         refs = [f"{object_name}.{mapping[arg]}" for arg, val in kwargs.items() if val]
@@ -59,6 +60,11 @@ def create_object_get_endpoint(app: FastAPI, object_name: str, attrs: list[str])
     }
     app.add_api_route(f"/{object_name}/get/", object_get_endpoint(object_name, endpoint_params), methods=["GET"])
 
+"""
+This function queries the simulation for it's api and adds two endpoints for each object:
+* object_name/get/...: for each reference from the object, have an optional boolean parameter to include the field in returned results
+* object_name/set/...: for each writeable reference from the object, take an optional parameter to set the value.
+"""
 async def add_endpoints(app: FastAPI):
     api = await simServer.getAPI()
     for object, ref_dict in api.items():
